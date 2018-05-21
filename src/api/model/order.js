9f1f136d922cfc4ee26254298fedd75f712e2602
pointworld <onerf@sina.com>
2018-05-22 00:07:34 +0800

    auto update

--- src/api/model/order.js
+++ src/api/model/order.js
@@ -78,8 +78,13 @@ module.exports = class extends think.Model {
       case 0:
         statusText = '未付款'
         break
+    }
+    switch (orderInfo.pay_status) {
+      case 0:
+        statusText = '未付款'
+        break
       case 2:
-        statusText = '付款成功'
+        statusText = '已付款'
         break
     }
 
