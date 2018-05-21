cf17f05e06a12267d1b5b62697bc30fb0de0139a
pointworld <onerf@sina.com>
2018-05-21 16:05:55 +0800

    auto update

--- src/common/config/config.js
+++ src/common/config/config.js
@@ -7,8 +7,8 @@
 module.exports = {
   default_module: 'api',
   weixin: {
-    appid:  'wxcbe1d349cf9edcaa', // 小程序 appid
-    secret: 'f4b4b907fda757803c36109f14d01354', // 小程序密钥
+    appid:  'wxf8fcfcff306bd31a', // 小程序 appid
+    secret: '4fcf16b33dcef420bd3ccd95afe785d8', // 小程序密钥
     mch_id: '1499922042', // 商户帐号ID
     partner_key: 'asjdfnwefjnwksegnasegjsangejgeww', // 微信支付密钥
     notify_url: 'https://runfa.sciclean.cn/api/pay/notify' // 微信异步通知，例：https://runfa.sciclean.cn/api/pay/notify
